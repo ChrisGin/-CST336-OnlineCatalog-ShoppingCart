6490a8b05ba025a37234062504a7d8da2b9257f4
vill2101 <savillavicencio@csumb.edu>
2016-04-12 21:54:44 +0000

    Cart visibility

--- cart.php
+++ cart.php
@@ -8,16 +8,21 @@
     {
         global $dbConnection;
         $total = 0;
-        echo "<table><tr><th>Product Name</th><th>Quantity</th><th>Unit Price</th></tr>";
-        foreach($_SESSION['cart'] as $id => $item){
-            echo "<tr>";
-            echo "<td>$item[productName]</td>";
-            echo "<td>$item[qty]</td>";
-            echo "<td>$item[price]</td></tr>";
-            $total += ($item['price'] * $item['qty']);
+        if( isset($_SESSION['cart']) ) {
+            echo "<table><tr style='background:white;'><th>Product Name</th><th>Quantity</th><th>Unit Price</th></tr>";
+            foreach($_SESSION['cart'] as $id => $item){
+                echo "<tr style='background:white'>";
+                echo "<td>$item[productName]</td>";
+                echo "<td>$item[qty]</td>";
+                echo "<td>$item[price]</td></tr>";
+                $total += ($item['price'] * $item['qty']);
+            }
+            echo "</table>";
+            echo "<p style='background:white;width:100px;'>$$total</p>";
         }
-        echo "</table>";
-        echo $total;
+        else {
+            echo "<h3 style='color:white;'>Cart is empty</h3>";
+        }
     }
 ?>
 
